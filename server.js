1ba31c6c28077194e7363a34d3694ec3cbf1bb95
ShAdOwCoDeXYT <nikilstudys@gmail.com>
2021-04-15 10:19:37 -0700

    Done with Server.js

--- server.js
+++ server.js
@@ -20,3 +20,66 @@ const app = express()
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body
+    const user = await User.findOne({ username }).lean()
+
+    if(!user) {
+        return res.json({ status: 'error', error: 'Invalid username/password' })
+    }
+
+    if(await bcyrpt.commpare(password, user.password)) {
+        const token = jwt.sign(
+            {
+                id: user._id,
+                username: user.username
+            },
+            JWT_SECRET
+        )
+
+        return res.json({ status: 'ok', data: token })
+    }
+
+    res.json({ status: 'error', error: 'Invalid username/password' })
+})
+
+app.post('/api/register', async (req, res) => {
+    const { username, password, plainTextPassword } = req.body
+
+    if(!username || typeof username !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid username' })
+    }
+
+    if(!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid Password' })
+    }
+
+    if(plainTextPassword.length < 5) {
+        return res.json({
+            status: 'error',
+            error: 'Password to small should atleast be 6 characters'
+        })
+    }
+
+    const password = await bcrypt.hash(plainTextPassword, 10)
+
+    try {
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log('User created successfully: ', response)
+    } catch (err) {
+        if(error.code === 11000) {
+            //duplicate key
+            return res.json({ status: 'error', error: 'Username already in use' })
+        }
+        throw error
+    }
+
+    res.json({ status: 'ok' })
+})
+
+app.listen(9999, () => {
+    console.log('Server up at 9999')
+})
